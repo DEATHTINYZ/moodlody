59fdb1716735db6d5107eb2f824bcc9ef6af3e97
DEATHTINYZ <78017906+DEATHTINYZ@users.noreply.github.com>
2023-01-14 23:39:35 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
+import Footer from '@/components/footer/Footer'
+import SoundPlayer from '@/components/sound/SoundPlayer'
+
 export default function Home() {
   return (
-    <main className="">
-      <div>test</div>
+    <main className="w-ful h-full">
+      <div className="flex flex-col justify-center items-center gap-[2rem] container">
+        <div className="pt-[4rem] text-center text-white">
+          <div className="text-[32px] sm:text-[48px] lg:text-[72px] font-bold">
+            Moodlody
+          </div>
+          <div>
+            “Moodlody is an app with nature sounds to help you focus, sleep,
+            relax and relieve stress.”
+          </div>
+        </div>
+        <div>
+          <SoundPlayer />
+        </div>
+      </div>
+      <Footer />
     </main>
   )
 }
