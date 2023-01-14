dd5e552cd1bb5a3de7de9a5620771354e81a763d
DEATHTINYZ <78017906+DEATHTINYZ@users.noreply.github.com>
2023-01-14 23:39:33 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import '@fontsource/roboto-mono'
 import '../styles/globals.css'
 
 export default function RootLayout({
@@ -8,7 +9,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head />
-      <body>{children}</body>
+      <body className="bg-gradient-to-r from-sky-500 to-indigo-500">
+        {children}
+      </body>
     </html>
   )
 }
