a3f4c25f549519a22281d121404c27edc532f0fc
DEATHTINYZ <78017906+DEATHTINYZ@users.noreply.github.com>
2023-01-15 18:36:58 +0700

    Create Footer.tsx

---
+++ components/footer/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="sticky w-full text-center p-[1rem] text-white">
+      <div>
+        Made with 💗 by{' '}
+        <a href="https://github.com/DEATHTINYZ/">Dream.Chayutpong</a>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
