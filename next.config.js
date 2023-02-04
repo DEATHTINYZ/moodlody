4873303af963eec0d5a6ef83afad2c973ebc69fb
DEATHTINYZ <78017906+DEATHTINYZ@users.noreply.github.com>
2023-02-04 17:15:21 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const withPWA = require('next-pwa')({
   register: true,
   skipWaiting: true,
   runtimeCaching,
-  disable: process.env.NODE_ENV === 'development',
+  disable: isDev,
   exclude: [
     // add buildExcludes here
     ({ asset, compilation }) => {
