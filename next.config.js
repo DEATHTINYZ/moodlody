eca1bd6a0a93af5f3c7334dd165fb4c745d9953d
DEATHTINYZ <78017906+DEATHTINYZ@users.noreply.github.com>
2023-02-04 13:14:57 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,12 +3,14 @@ const runtimeCaching = require("next-pwa/cache");
 const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
-  skipWaiting: false,
+  skipWaiting: true,
   runtimeCaching,
-  disable:process.env.NODE_ENV === 'development'
+  disable:process.env.NODE_ENV === 'development',
+  buildExcludes: [/middleware-manifest.json$/]
 });
 
 const nextConfig = withPWA({
+  reactStrictMode: true,
   experimental: {
     appDir: true,
   },
